1d0261a23ec0e672f3ce5b7aaf85aff8b2f4cf4a
Wulfheart <dev@alexfwulf.de>
2020-05-01 00:01:19 +0200

    Comments for init created

--- cmd/init.go
+++ cmd/init.go
@@ -1,10 +1,24 @@
 package cmd
 
-import "github.com/urfave/cli/v2"
+import (
+	"github.com/urfave/cli/v2"
+)
 
 var initCmd = &cli.Command{
-	Name:      "init",
-	Action:    nil,
+	Name: "init",
+	Action: func(c *cli.Context) error {
+		//  check if directory name already exists
+		// Download template project (https://github.com/System-Glitch/goyave-template/archive/master.zip)
+		// unzip template
+		// move content to folder
+		// copy config.example.json to config.json
+		// Initialize git
+
+		//! We need another template repo as I don't want to use the find function
+
+		//
+		return nil
+	},
 	Usage:     "Initializes a new project",
 	UsageText: "Text is a little bit longer",
 }
